a1ebe0d2bc0540e968183a6908446390c0dab936
Jeff B <dukedvl@gmail.com>
2023-02-24 01:05:16 -0600

    routes

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,36 @@ use std::thread;
 use std::time::Duration;
 
 extern crate chrono;
+#[macro_use]
 extern crate nickel;
 
 mod weather;
 
 fn main() {
-    let a = weather::DailyWeather::default();
+    //Wakeup
 
-    println!("Hello, world!");
+    //Start Cache refresh Loop
+
+    //WeatherCacheLoop
+    //if Now - LastInMemInstantPull > 30min,
+    //PullInstWeb
+    //RefreshTimestamps
+    //if Now - LastDBHourlyPull > 6hr,
+    //PullHourlyWeb
+    //RefreshTimestamp
+    //PersistHourlyData
+    //if Now - LastDailyPull > 24h,
+    //PullDailyWeb
+    //RefreshTimestamp
+    //PersistDailyData
+
+    let mut server = Nickel::new();
+
+    //Weather routes
+    server.get("/forecast/daily", middleware! {"daily"});
+    server.get("/forecast/hourly", middleware! {"hourly"});
+    server.get("/forecast/instant", middleware! { "instant"});
+    server.get("/historical", middleware! {"historical"});
+
+    server.listen("127.0.0.1:3031").unwrap();
 }
